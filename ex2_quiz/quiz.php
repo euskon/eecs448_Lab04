3c4dd2f10d16d2d9c4f62f983faf3e8d8ac3da40
Zach Freund <theeuskon@gmail.com>
2019-02-28 23:30:52 -0600

    inital php quiz check attemp 2 for loops.

---
+++ ex2_quiz/quiz.php
@@ -0,0 +1,19 @@
+<?php
+  $score = 0;
+  $attempted = 5;
+  $ansKey = array("regular", "regular", "regular", "regular", "regular");
+  $ansInput = array();
+  for($i = 0; $i < 5; $i++){
+    $ansInput[$i] = $_POST["qa" . ($i+1)];
+    //catches empty case error
+    if(empty($ansInput[$i])){
+      $attempted--;
+    } elseif($ansKey[$i] = $ansInput[$i]){
+      $score++;
+    }
+  }
+  echo "<h1>YOUR SCORE IS: " . ($score / 5) . "</h1><br>";
+  echo "<h2>Number of Attempts: " . $attempted . "</h2><br>";
+
+
+ ?>

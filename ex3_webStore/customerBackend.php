40844d2e6d2c665184bc354afecbeafe9807908b
Zach Freund <theeuskon@gmail.com>
2019-03-04 14:08:11 -0600

    init html, js, css, and php sources. Also created form in html almost complete.

---
+++ ex3_webStore/customerBackend.php
@@ -0,0 +1,5 @@
+<!-- customerBackend.php
+Processes the purchase
+- Prints a welcome message to the user 
+- displays their password (never do this in production!, this is only practice to get used to transmitting form information)
+Prints a receipt -->
---
+++ ex3_webStore/formChecker.js
@@ -0,0 +1,7 @@
+// formChecker.js
+// Validates the input from the customer before submitting
+// - Quantities >= 0, cannot be blank
+// - User name must be in the form of an email address (name@domain.com)
+// - password field cannot be blank
+// - must pick a shipping option
+// We do not have a database to check against, the password can by anything

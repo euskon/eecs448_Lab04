8136f39f017fb1b141c01634e5cdac1fa2a4facb
Zach Freund <theeuskon@gmail.com>
2019-03-06 00:07:32 -0600

    php that was forgotten.

--- ex3_webStore/customerBackend.php
+++ ex3_webStore/customerBackend.php
@@ -1,5 +1,10 @@
 <!-- customerBackend.php
 Processes the purchase
-- Prints a welcome message to the user 
+- Prints a welcome message to the user
 - displays their password (never do this in production!, this is only practice to get used to transmitting form information)
 Prints a receipt -->
+<?php
+  echo "<h1>Welcome:" . $_POST["username"] . "</h1><br>";
+  echo "<h6>For Lab Example Purposes Only</h6><br>";
+  echo "PW: " . $_POST["password"] . "<br>";
+ ?>

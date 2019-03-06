d106d805fe427588fb92d67635b68c855243a977
Zach Freund <theeuskon@gmail.com>
2019-03-06 11:34:04 -0600

    receipt in progress.

--- ex3_webStore/customerBackend.php
+++ ex3_webStore/customerBackend.php
@@ -4,7 +4,9 @@ Processes the purchase
 - displays their password (never do this in production!, this is only practice to get used to transmitting form information)
 Prints a receipt -->
 <?php
+  echo "<link href="style.css" rel="stylesheet" type="text/css"/>";
   echo "<h1>Welcome:" . $_POST["username"] . "</h1><br>";
-  echo "<h6>For Lab Example Purposes Only<br>";
-  echo "PW: " . $_POST["password"] . "</h6><br>";
+  echo "<h2>PW: " . $_POST["password"] . "</h2><br>";
+  echo "<h3>Receipt for Purchase: </h3><br>";
+  echo "<table><th>Item"
  ?>
